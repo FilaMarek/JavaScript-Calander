6ee92669b6ffb9bc40371dc2a4d6491561247482
FilaMarek <48298031+FilaMarek@users.noreply.github.com>
2019-08-13 22:10:10 +0200

    Days in month correctly counted
    when manually in a code changing the date the days are counted correctly, now i need to make sure buttons on the calader do the same thing.

--- script.js
+++ script.js
@@ -3,26 +3,13 @@
 
 var currentMonths = new Date().getMonth();// what month it is
 var Months = currentMonths;
-var year = new Date().getFullYear();
+var year =  new Date().getFullYear();
 var days = new Date().getDate();//day of the month ex 11
 var daysOfTheweek = new Date().getDay(); // days of the week ex 6 === sat
 var weekNumber = 0;
+var tempMonths = Months +1;
 
-var January = 31;//0
-var February = 28;
-var February2 = 29;
-var March = 31;//2
-var April = 30;
-var May = 31;//4
-var June = 30;
-var July = 31;//6
-var August = 31;//7
-var September =30;
-var October = 31;//9
-var November = 30;
-var December = 31;//11
-
-
+var dateobj =  new Date(tempMonths  +" " + 1 + " " + year)
 
 
 
@@ -33,22 +20,21 @@ var setUpDate = function () /// function on load
     monthTranslator();  
     countingDays();
 }
-
 var nextMonth = function() // button next month
 {
+    eraseHTML();
     if (Months < 11)// if Month is not December
     {
     Months = Months + 1;
         monthTranslator();
+// function that will add months and re assign the date obj
+        monthSetup(); 
     }
     else {
         Months = 0;
         year = year + 1;
         monthTranslator();
     }
-    
-
-    
 };
 
 var lastMonth = function()// button previouse month
@@ -63,7 +49,6 @@ var lastMonth = function()// button previouse month
         year = year - 1;
         monthTranslator();
     }
-    
 };
 
 
@@ -137,10 +122,6 @@ var countingDays = function()
    }
     
 };
-
-
-  
-  
        var nextDays = daysOfTheweek;
        var tempWeekNumb= 1;
 
@@ -148,23 +129,22 @@ var countingDays = function()
   var monthSetup = function()
   {
       
-      dateobj = new Date('Jan 1, 2000 ');
       year= dateobj.getFullYear();
       days= dateobj.getDate();
       currentMonths= dateobj.getMonth();
       daysOfTheweek= dateobj.getDay();
-      console.log(year);
-      console.log("day of the month"+days);// what day of the month ex 29th
-      console.log("current month number"+currentMonths);// 0 to January 11 to December
-      console.log("what days of  week"+daysOfTheweek);// first day of 2000 is na 1st Saturday
+      //console.log(year);
+      //console.log("day of the month "+days);// what day of the month ex 29th
+      //console.log("current month number "+currentMonths);// 0 to January 11 to December
+     // console.log("what days of  week "+daysOfTheweek);// first day of 2000 is na 1st Saturday
      
-      if(daysOfTheweek === 6)
+      if(daysOfTheweek ===6)
           {
               
              document.getElementById("tableTop"+ tempWeekNumb/*week*/+"-"+daysOfTheweek/*day*/).innerHTML = days; 
               
-              tempWeekNumb = 2;
-              days = 2;
+              tempWeekNumb++;
+              days++;
               for(tempWeekNumb;tempWeekNumb<7;tempWeekNumb++){
               for(daysOfTheweek=0;daysOfTheweek<=6;daysOfTheweek++)
                   {
@@ -172,33 +152,50 @@ var countingDays = function()
                     document.getElementById("tableTop"+ tempWeekNumb/*week*/+"-"+daysOfTheweek/*day*/).innerHTML = days;  
                      days++; 
                       if((currentMonths === 0 || currentMonths === 2 || currentMonths === 4 || currentMonths === 6 || currentMonths === 7 || currentMonths === 9 || currentMonths === 11) && days <= 31)
-                            {
-
-                            }
+                            {}
                       else if((currentMonths === 3 || currentMonths === 5 || currentMonths === 8 || currentMonths === 10) && days <= 30)
-                      {  
-                          
-                      }else {break;}
-                      
-                      
-                      
-                  }
-              } 
-              
-          }
+                      {}else {break;}
+                  }// for days
+              }  // for weeks
+          }// if first dayy of the month is a saturday
+            else if(daysOfTheweek < 6)
+                {
+                   for(tempWeekNumb;tempWeekNumb<6;tempWeekNumb++){
+                     for(daysOfTheweek;daysOfTheweek<=6;daysOfTheweek++){
+                         
+                        document.getElementById("tableTop"+ tempWeekNumb/*week*/+"-"+daysOfTheweek/*day*/).innerHTML = days;  
+                         days++;
+                            if(daysOfTheweek === 6)
+                                {
+                                    daysOfTheweek = 0;
+                                    break;
+                                }
+                         if((currentMonths === 0 || currentMonths === 2 || currentMonths === 4 || currentMonths === 6 || currentMonths === 7 || currentMonths === 9 || currentMonths === 11) && days <= 31)// all months with 31 days
+                            {}else if((currentMonths === 3 || currentMonths === 5 || currentMonths === 8 || currentMonths === 10) && days <= 30)// alse months with 30 days
+                              {}else if((currentMonths === 1 && (year % 4)=== 0) &&  days <=29) // Feb with 29 days every 4 years
+                                 {}else if((currentMonths === 1 && (year % 4)> 0) &&  days <=28)// Feb with 28 days other years
+                                     {}else {break;} 
+                         
+                     }  // for days of week
 
-  };
+                   } // for temp week
+                    
+                }//end of else if
+  };// end of function calander
 
-  /*
-var February = 28;//1
-var February2 = 29;//1(2)
 
-var April = 30;//3
 
-var June = 30;//5
 
-var September =30;//8
-
-var November = 30;//10
-
-  */
+var eraseHTML = function()
+{
+    var tempVar = tempWeekNumb =1;
+    var tempVar2 = daysOfTheweek=0;
+    
+                  for(tempVar;tempVar<7;tempVar++){
+              for(tempVar2=0;tempVar2<=6;tempVar2++)
+                  {
+                  
+                    document.getElementById("tableTop"+ tempVar/*week*/+"-"+tempVar2/*day*/).innerHTML = " ";   
+                  }// for days
+              }  // for weeks
+}
